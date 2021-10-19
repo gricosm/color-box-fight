f6c7b76de86e79e5deaa8b06c706573bb2adef68
gricosm <guillermo.rico.sm@gmail.com>
2021-10-19 21:19:01 +0200

    Añadida la clase principal para la selección de personajes.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
-// Defino las variables globales.
+// Defino las clases generales que usaré el juego.
+// Clase Character se ocupará de definir a los personajes del juego.
 
-
-function startGame(){
-     
+class Character {
+    constructor(characterName, characterHealth, characterAttack){
+        this.characterName = characterName
+        this.characterHealth = characterHealth
+        this.characterAttack = characterAttack
+    }
 }
 
-function attack(){
+const pinkHero = new Character ("Pink", 10, 3)
+const yellowHero = new Character ("Yellow", 15, 2)
+const redHero = new Character ("Red", 12, 2)
+const blueHero = new Character ("Blue", 7, 4)
 
-}
 
-function finishGame(){
 
-}
+// class Player {
+//     constructor(playerName, character) {
+//         this.player = playerName
+//         this.character = character
+//     }
+// }
--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 ¡Bienvenido, bienvenida y bienvenide al mejor y más asombroso juego de acción entre cubos de colores!*
 
 *Puede que esta información no sea del todo verídica.
-
