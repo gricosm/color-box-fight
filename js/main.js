ce68ad9123ab0ea4fe05b21679d671aa6049b975
gricosm <guillermo.rico.sm@gmail.com>
2021-10-21 23:07:55 +0200

    Hecho el capturar con clicks

--- js/main.js
+++ js/main.js
@@ -3,4 +3,43 @@ const blueFighter = '<button><img src="media/img/bluehero-on.jpg" alt="Héroe Az
 const redFighter = '<button><img src="media/img/redhero-on.jpg" alt="Héroe Rojo"></button>'
 const pinkFighter = '<button><img src="media/img/pinkhero-on.jpg" alt="Héroe Rosa"></button>'
 
-let chooseFighter = (player) => document.getElementById("playerFight").innnerHTML = (player)
+const chooseFighter = (player) => {
+
+    console.log('parametro ', player)
+    
+    switch(player) {
+        case "yellowFighter":
+            document.getElementById("playerFight").innerHTML = yellowFighter
+            break
+        case "blueFighter":
+            document.getElementById("playerFight").innerHTML = blueFighter
+            break
+        case "redFighter":
+            document.getElementById("playerFight").innerHTML = redFighter
+            break
+        case "pinkFighter":
+            document.getElementById("playerFight").innerHTML = pinkFighter
+            break        
+    }
+}
+
+let doneClicks = 0
+let clickedButtons = document.getElementsByClassName("character")
+
+let incrementoClick = () => {
+
+    doneClicks += 1
+    
+    if(doneClicks == 2){
+        console.log("hemos hecho dos clicks!!!!")
+    }
+
+}
+
+clickedButtons[0].addEventListener("click", incrementoClick)
+clickedButtons[1].addEventListener("click", incrementoClick)
+
+
+// 1. Saber los clicks que se hacen y parar cuando nº de clicks == 2
+// 2. Dibujar dos diferentes:  ¿Marcar dos <div> en los que introducir los button para la segunda parte del juego?
+// 3. Ocultar sección de elegir: modificar el HTML ¿borrando? lo que había y dibujando lo anterior (Aprovechar un if clicks == 2)
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ¡Bienvenido, bienvenida y bienvenide al mejor y más asombroso juego de acción entre cubos de colores!*
 
 *Puede que esta información no sea del todo verídica.
+
